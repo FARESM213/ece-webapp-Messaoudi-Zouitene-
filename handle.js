a8f18021bf778869503f0c581e1323bb8c9d26f1
FARESM213 <fares.messaoudi@edu.ece.fr>
2022-10-11 21:34:14 -0400

    Version sans Readme

--- handle.js
+++ handle.js
@@ -1,58 +1,118 @@
 const url = require('url')
 const qs = require('querystring')
 const about = require('./content/about.json')
-
 const app=require("express").Router();
+const { v4: uuidv4 } = require('uuid');
 
-module.exports = {
-
-    serverHandle: function (req, res) {
-      
-            const route = url.parse(req.url)
-            const path = route.pathname 
-            const params = qs.parse(route.query)
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-
-           /* app.use('/', function (req, res) {
-                
-                console.log('ertyuiol');
-                res.send('Bonjour Bienvenue')
-            
-              })
-            
-            app.get('/about', (req, res) => {
-
-                res.write(JSON.stringify(about))  
-               // res.send('Bonjour Bienvenue')
-
-            })*/
-              
-              
-           if (path === '/hello' && 'name' in params) 
-            {
-                if (params['name']=="Fares")
-                {
-                    res.write("Fares wesh bg ");
-                }
-                else
-                {
-                    res.write('Hello ' + params['name'])
-
-                }
-            } 
-            else if (path ==='/hello')
-            {
-                res.write('Pour utiliser votre name, vous devez entrer "/hello?name= "votre nom" ')
-            }
-            else if (path === '/about')
-            {          
-              res.write(JSON.stringify(about))  
-            }
-            else if (path==="/")
-            {
-                res.write('Bonjour Bienvenue')
-            }  
-            res.end();
-    } 
-
+let db = {
+    articles: [
+      {
+        id: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        title: 'My article',
+        content: 'Content of the article.',
+        date: '04/10/2022',
+        author: 'Liz Gringer'
+      },
+      // ...
+    ],
+    comments: [
+      {
+        id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+        timestamp: 1664835049,
+        content: 'Content of the comment.',
+        articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        author: 'Bob McLaren'
+      },
+      {
+        id: '9b1dfb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+        timestamp: 1664835049,
+        content: 'Content of the commentdh eh ceh che ehc eh.',
+        articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        author: 'Bob McLaren'
+      },
+      // ...
+    ]
   }
+
+app.get('/hello',(req,res)=>{
+    res.send("hello word");
+});
+
+app.get('/',(req,res)=>{
+    res.send("Bonjour Bienvenue'");
+});
+
+app.get('/about',(req,res)=>{
+    res.send(JSON.stringify(about))  
+});
+
+app.get('/hello/name=:username', (req, res) => {
+    var name=req.params.username.toString()
+    if(name==="Fares"){  
+        res.send(" Fafiso le bg ");
+    }
+    else {
+        res.send(" Hello "+ name);
+    }
+  })
+
+  /***************** ARTICLES ******************/
+app.get("/articles", (req, res) => {
+
+    res.send(db.articles);
+  });
+
+  app.post("/articles", (req, res) => {
+
+    console.log(req.body);
+    db.articles.push({...req.body,id : uuidv4()});
+    res.send('POST SUCCED ');
+
+  });
+
+  app.get("/articles/:articleId", (req, res) => {
+    res.send(db.articles.find((article) => article.id === req.params.articleId));
+  })
+    
+  /***************** Comments  ******************/
+  
+  app.get("/articles/:articleId/comments", (req, res) => {
+
+    const comments = [];
+    for (var i = 0; i < db.comments.length; i++) {
+      if (req.params.articleId === db.comments[i].articleId) {
+        comments.push(db.comments[i]);
+      }
+    }
+    res.send(comments);
+
+  });
+
+  app.post("/articles/:articleId/comments", (req, res) => {
+
+    console.log(req.body);
+    db.comments.push({...req.body,id : uuidv4(), timestamp : Date.now()})
+    res.send("POST SUCCED");
+
+  });
+
+  app.get("/articles/:articleId/comments/:commentId", (req, res) => {
+
+    const resultat = [];
+  
+    for (var i = 0; i < db.comment.length; i++) 
+    {
+
+      if ((req.params.commentId === db.comment[i].id)&&(req.params.articleId===db.comments[i].articleId)) 
+      {
+        resultat.push(db.comment[i]);
+      }
+
+    }
+    res.send(resultat);
+
+    });
+
+
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const handles = require('./handle')
 var express = require('express');
-var app = express();
+const bodyParser = require("body-parser");
+
 var PORT = 8080;
-app.use('/',handles.serverHandle);
- 
+var app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(handles);
 app.listen(PORT, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", PORT);
