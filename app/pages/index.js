e3e469c667feaad798dcc7014fa3c345b73cdcb6
FARESM213 <fares.messaoudi@edu.ece.fr>
2022-10-18 22:47:21 -0400

    Lab4 sans ReadME

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,42 +14,33 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         Bienvenue sur la HomePage en <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <br></br>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <Link href="/about"> 
+           <a className={styles.card}>
+            <h2>About &rarr;</h2>
+            <p>Sur cette page, retrouvez toute les informations nous concernant.</p>
           </a>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <Link href="/contacts"> 
+          <a className={styles.card}>
+            <h2>Contacts &rarr;</h2>
+            <p>Sur cette page, veuillez retrouvez tout nos contacts</p>
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          </Link>
+          
+          <Link href="/articles"> 
+          <a className={styles.card}>
+            <h2>Articles &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Cette page contiendra la liste effectives de tout nos articles.
             </p>
           </a>
+          </Link>
         </div>
       </main>
 
