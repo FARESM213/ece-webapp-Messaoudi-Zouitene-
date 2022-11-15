d6d97dd4a61f868ae4ab3335e7a24eee4ecef940
Lydiaa21 <axel@aquelito.fr>
2022-11-15 10:00:59 +0100

    changement code css en css T

--- app/pages/amerique.js
+++ app/pages/amerique.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/s.module.css'
 import Footer from '../content/footer.js'
 import Header from '../content/header'
 
