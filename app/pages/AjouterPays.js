c16f9f87d4c7516e8db62ef106dcce4604dab214
FARESM213 <fares.messaoudi@edu.ece.fr>
2022-12-19 17:09:30 +0100

    Insert problem fixed

--- app/pages/AjouterPays.js
+++ app/pages/AjouterPays.js
@@ -1,53 +1,69 @@
-import styles from '../styles/Home.module.css'
-import supabase from '../supabase'
-import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import {getServiceSupabase} from '../supabase'
 import UserContext from '../components/UserContext'
 import { useContext } from 'react'
+import { useRouter } from 'next/router'
+import styles from '../styles/Home.module.css'
+
+const supabase=getServiceSupabase();
 
 
-export default function AjouterPays() {
+export default function Profile({ equipe }) {
 
-    const router = useRouter()
-    const { user, logout, loading } = useContext(UserContext)
-    const compo = ['3-1-4-2', '3-4-1-2', '3-4-2-1','3-4-3','3-5-1-1','3-5-2','4-1-2-1-2','4-1-3-2' ,'4-1-4-1','4-2-2-2','4-2-3-1','4-2-4','4-3-1-2','4-3-2-1','4-3-3','4-4-1-1','4-4-2','4-5-1','5-2-1-2','5-2-2-1','5-2-3','5-3-2','5-4-1'];
-    const continent = ['Amérique', 'Asie', 'Afrique', 'Europe'];
+  const { user, logout, loading } = useContext(UserContext)
 
-    async function handleSubmit(event) {
-        
-        event.preventDefault();
-        const equipe = event.target.name.value;   
-        const Entraineur = event.target.Coatch.value;
-        const compo = ""+document.querySelector('#compo').value;
-        const victoire = event.target.anglais.value;
-        const continen = ""+document.querySelector('#continent').value;
-        const players = event.target.joueurs.value;
+  const [data, setData] = useState()
+  const router = useRouter()
 
-        /// Recuperer les valeurs du  reste des formes de la meme maniere que juste au dessus
-
-        const {data,error}= await supabase.from('equipe').select("*").eq('nom', equipe).single()
-
-        if(data==null)
-        {
-                const { error } = await supabase.from('equipe').insert({ nom: equipe, coach:Entraineur,flag:victoire,user_id: user.id, Composition:compo,Continent:continen,Liste_joueurs:players}) /// Ici juste assigner les valeurs au truck de la base de donnes ( par exemple dans la bdd le nom de lequipe c'est "nom" et je lui donne la valeur "equipe")
-               
-                if(error) 
-                {
-                    alert("Une erreur est survenue lors de l'insertion de votre equipe")
-                }
-                else
-                {
-                    alert("L'equipe a bien été ajoutée")
-                    router.push('/equipe')
-                }
-
-        }else
-        {
-            alert("L'equipe est deja dans la bdd")
-        }
+  const compo = ['3-1-4-2', '3-4-1-2', '3-4-2-1','3-4-3','3-5-1-1','3-5-2','4-1-2-1-2','4-1-3-2' ,'4-1-4-1','4-2-2-2','4-2-3-1','4-2-4','4-3-1-2','4-3-2-1','4-3-3','4-4-1-1','4-4-2','4-5-1','5-2-1-2','5-2-2-1','5-2-3','5-3-2','5-4-1'];
+  const continent = ['Amérique', 'Asie', 'Afrique', 'Europe'];
+  async function handleSubmit(event) {
       
-      }
-  return (
+      event.preventDefault();
+      const equipe = event.target.name.value;   
+      const Entraineur = event.target.Coatch.value;
+      const compo = ""+document.querySelector('#compo').value;
+      const victoire = event.target.anglais.value;
+      const continen = ""+document.querySelector('#continent').value;
+      const players = event.target.joueurs.value;
+      const {data,error}= await supabase.from('equipe').select("*").eq('nom', equipe).single()
 
+      if(data==null)
+      {
+              const { error } = await supabase.from('equipe').insert({ 
+
+                  nom: equipe, 
+                  coach:Entraineur,
+                  flag:victoire,
+                  user_id: user.id, 
+                  Composition:compo,
+                  Continent:continen,
+                  Liste_joueurs:players
+                  
+              
+              },{ returning: "minimal" })
+
+              if(error) 
+              {
+                  alert(JSON.stringify(error,"",2))
+                  alert("Une erreur est survenue lors de l'insertion de votre equipe")
+              }
+              else
+              {
+                  alert("L'equipe a bien été ajoutée")
+                  router.push('/equipe')
+              }
+
+      }else
+      {
+          alert("L'equipe est deja dans la bdd")
+      }
+    
+    }
+            return (
+
+          <div>
+        
         <form className={styles.addteamform} onSubmit={handleSubmit} >
 
             <label htmlFor="name">Nom de l'équipe :</label>
@@ -62,7 +78,7 @@ export default function AjouterPays() {
             <select  type="text" id="compo" name="compo">
             {compo.map(compo => (
             <option type="text" value={compo} key={compo}>{compo}</option>
-             ))}
+            ))}
             </select>
 
 
@@ -70,12 +86,13 @@ export default function AjouterPays() {
             <select  type="text" id="continent" name="continent">
             {continent.map(continent => (
             <option type="text" value={continent} key={continent}>{continent}</option>
-             ))}
+            ))}
             </select>
 
             <button type="submit">Ajouter l'équipe</button>
-        </form>
-  )
+      </form>
+              
+          </div>
   
+    )               
 }
-
