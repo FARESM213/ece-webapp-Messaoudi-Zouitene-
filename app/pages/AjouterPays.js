2cb6351082d7f55c89e5c639a264af071da11fd1
Lydiaa21 <axel@aquelito.fr>
2022-12-19 11:29:30 +0100

    formulaire d'ajout équipe fini

--- app/pages/AjouterPays.js
+++ app/pages/AjouterPays.js
@@ -16,53 +16,29 @@ export default function AjouterPays() {
       }
   return (
 
-    <div>
-<form onSubmit={handleSubmit}>
-
-<div className={styles.inputgroupA}>
-    <label>Nom de l'équipe</label>
-    <input type="text" name="équipe" /> <br/>
-   
-</div>
-
-
-<div className={styles.inputgroupA}>
-    <label>Composition</label>
-    <input type="text" name="Composition" /> <br/>
-   
-
-
-</div>
-
-<div className={styles.inputgroupA}>
-    <label>Entraineur</label>
-    <input type="text" name="Entraineur" /> <br/>
-
-
-</div>
-
-<div className={styles.inputgroupA}>
-    <label>Dernière victoire</label>
-    <input type="text" name="victoire" /> <br/>
-   
-
-</div>
-
-<div className={styles.inputgroupA}>
-<label>Continent</label>
+          <form className={styles.addteamform}>
+            <label htmlFor="name">Nom de l'équipe :</label>
+            <input type="text" id="name" name="name" required />
+            <label htmlFor="Coatch">Coatch :</label>
+            <input type="text" id="Coatch" name="Coatch" required />
+            <label htmlFor="anglais">Nom en anglais :</label>
+            <input type="text" id="anglais" name="anglais" required />
+            <label htmlFor="anglais">Listes des joueurs :</label>
+            <input type="text" id="joueurs" name="joueurs" required />
+            <label htmlFor="anglais">Composition équipe :</label>
+            <input type="text" id="Composition" name="Composition" required />
+            <label>Continent</label>
        <select name="categorie" className={styles.liste}>
            <option value="amerique" >Amérique</option>
            <option value="Europe">Europe</option>
            <option value="Asie">Asie</option>
            <option value="Afrique">Afrique</option>
        </select>
-       </div>
-
-    <div className={styles.inputgroupA}>
-    <button type="submit" name="Add" className={styles.btnA}>Ajouter</button>
-</div>
-</form>
-</div>
+            <label htmlFor="logo">Logo :</label>
+            <input type="file" id="logo" name="logo" accept="image/*" required />
+            <button type="submit">Ajouter l'équipe</button>
+          </form>
+      
 
   )
   
