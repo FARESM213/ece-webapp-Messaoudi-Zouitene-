83e5b809bc8496d3537b85a9e2e15c5b1b29af26
FARESM213 <fares.messaoudi@edu.ece.fr>
2022-12-23 00:52:54 +0100

    Footix

--- app/components/header.js
+++ app/components/header.js
@@ -15,8 +15,10 @@ export default function Header(props) {
             <div className={props.darkMode ?"header2":props.orangeMode?"header3":props.greenMode?"header4":props.blueMode?"header5":"header"}>
               <SignedIn/>  
               <a  className="logo" > <img src="../logoooo.png" width="200" ></img></a>       
+               <span className="footic font-semibold text-2xl tracking-tight"> Footix</span>
+
+            <div className="changement">              
             
-            <div className="changement">
                 <img onClick={props.toggleDarkMode} className="self-left justify-self-end" src={props.darkMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toggleOrangeMode} className="self-left justify-self-end" src={props.orangeMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toggleGreenMode} className="self-left justify-self-end" src={props.greenMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
@@ -39,9 +41,13 @@ export default function Header(props) {
     return (
       <header>
           <div className={props.darkMode ?"header2":props.orangeMode?"header3":props.greenMode?"header4":props.blueMode?"header5":"header"}>
-            <a  className="logo" > <img src="../logoooo.png" width="200" ></img></a>          <pre >Aucun utilisateur connecté </pre>
+            <a  className="logo" > <img src="../logoooo.png" width="200" ></img></a>          
+            <pre >Aucun utilisateur connecté </pre>            
+            
+            <span className="footic font-semibold text-xl tracking-tight"> Footix</span>
 
-            <div className="changement">
+            <div className="changement">            
+
                 <img onClick={props.toggleDarkMode} className="self-left justify-self-end" src={props.darkMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toggleOrangeMode} className="self-left justify-self-end" src={props.orangeMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toggleGreenMode} className="self-left justify-self-end" src={props.greenMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
