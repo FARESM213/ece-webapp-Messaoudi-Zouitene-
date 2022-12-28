67a7d075cde176822aa82d6abed0c219702d9ed6
FARESM213 <fares.messaoudi@edu.ece.fr>
2022-12-28 17:33:49 +0100

    Fix Bug 2

--- app/components/header.js
+++ app/components/header.js
@@ -13,17 +13,21 @@ export default function Header(props) {
     return (
         <header>
             <div className={props.darkMode ?"header2":props.orangeMode?"header3":props.greenMode?"header4":props.blueMode?"header5":"header"}>
-              <SignedIn/>  
-              <a  className="logo" > <img src="../logoooo.png" width="200" ></img></a>       
-               <span className="footic font-semibold text-2xl tracking-tight"> Footix</span>
+              <a  className="logo" > <img src="../logoooo.png" width="200" ></img></a>  
+            <div className="changement">                            
 
-            <div className="changement">              
-            
+               <span className="footic font-semibold text-2xl tracking-tight"> Footix</span> <br/> <br/>
                 <img onClick={props.toggleDarkMode} className="self-left justify-self-end" src={props.darkMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toggleOrangeMode} className="self-left justify-self-end" src={props.orangeMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toggleGreenMode} className="self-left justify-self-end" src={props.greenMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toogleBlueMode} className="self-left justify-self-end" src={props.blueMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
             </div>
+            <div className='user'>
+
+                <SignedIn/>  
+
+            </div>
+
                 <div className={props.darkMode ?"header2-right":"header-right"}>
 
                     <Link className="active" href="/" >Home </Link>
@@ -41,18 +45,17 @@ export default function Header(props) {
     return (
       <header>
           <div className={props.darkMode ?"header2":props.orangeMode?"header3":props.greenMode?"header4":props.blueMode?"header5":"header"}>
-            <a  className="logo" > <img src="../logoooo.png" width="200" ></img></a>          
-            <pre >Aucun utilisateur connecté </pre>            
-            
-            <span className="footic font-semibold text-xl tracking-tight"> Footix</span>
-
+            <a  className="logo" > <img src="../logoooo.png" width="200" ></img></a>             
             <div className="changement">            
-
+               <span className="footic font-semibold text-xl tracking-tight"> Footix</span><br/> <br/>
                 <img onClick={props.toggleDarkMode} className="self-left justify-self-end" src={props.darkMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toggleOrangeMode} className="self-left justify-self-end" src={props.orangeMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toggleGreenMode} className="self-left justify-self-end" src={props.greenMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
                 <img onClick={props.toogleBlueMode} className="self-left justify-self-end" src={props.blueMode ? "../ON.png" : "../OFF.png"} width="75" height={"38"} />
             </div>
+            <div className='user'>
+                <pre >Aucun utilisateur connecté</pre>       
+            </div>    
               <div className={props.darkMode ?"header2-right":"header-right"}>
  
                   <Link className="active" href="/" >Home </Link>
